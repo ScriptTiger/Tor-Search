45e849c8fe94c361d6594c746e9943ad241aabc2
ScriptTiger <thescripttiger@gmail.com>
2019-03-15 00:11:13 +0800

    Added header comment block

--- Tor_Search.cmd
+++ Tor_Search.cmd
@@ -1,4 +1,13 @@
 @echo off
+
+rem =====
+rem For more information on ScriptTiger and more ScriptTiger scripts visit the following URL:
+rem https://scripttiger.github.io/
+rem Or visit the following URL for the latest information on this ScriptTiger script:
+rem https://github.com/ScriptTiger/Tor-Search
+rem =====
+
+rem Enable delayed expansion
 setlocal ENABLEDELAYEDEXPANSION
 
 rem =====
